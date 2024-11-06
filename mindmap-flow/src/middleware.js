2f3a6d3aa8a14050dfba0bf69e2ef8e22d1d8de0
Michael Nguyen <ngxphong03@gmail.com>
2024-11-06 16:28:50 +0700

    update

--- mindmap-flow/src/middleware.js
+++ mindmap-flow/src/middleware.js
@@ -2,20 +2,20 @@ import { NextResponse } from "next/server";
 import { getSession } from "@auth0/nextjs-auth0/edge";
 
 export async function middleware(request) {
-  const response = new NextResponse();
+  // const response = new NextResponse();
   const pathname = request.nextUrl.pathname;
   const id = pathname.replace("/mindmap", "").replace("/", "");
   const mindmapResponse = await fetch(
     `${process.env.CLIENT_BASE_URL}/api/mindmap?id=${id}`
   );
-  const mindmapData = await mindmapResponse.json();
-  const user = await getSession(request, response);
-
+  const mindmap = await mindmapResponse.json();
   let mode = "private";
-  if (mindmapData?.data?.public) {
+  // const user = await getSession(request, response);
+  if (mindmap?.data?.public) {
     mode = "public";
   }
   if (mode === "private") {
+    const user = await getSession();
     if (!user) {
       return NextResponse.redirect(new URL("/api/auth/login", request.url));
     }
@@ -25,5 +25,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/mindmap/:id"],
+  matcher: ["/mindmap/:id*"],
 };
