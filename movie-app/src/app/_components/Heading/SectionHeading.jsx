104d3664f94ec2bd3e905f1dcec6c56895d7f0d0
Michael Nguyen <ngxphong03@gmail.com>
2024-11-11 14:08:19 +0700

    update

--- movie-app/src/app/_components/Heading/SectionHeading.jsx
+++ movie-app/src/app/_components/Heading/SectionHeading.jsx
@@ -1,6 +1,6 @@
 export default function SectionHeading({ title }) {
   return (
-    <h2 className="text-xl font-bold tracking-wide lg:text-2xl flex items-center gap-2 text-light dark:text-white">
+    <h2 className="text-xl font-bold lg:text-2xl flex items-center gap-2 text-light dark:text-white">
       <span className="w-2 h-6 lg:h-8 bg-[#032541] block rounded-full"></span>{" "}
       {title}
     </h2>
